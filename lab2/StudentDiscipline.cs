c9e34a4118202e4834335b5dbcd1a4f353885ab7
Vadim Kruposiy <90095400+krvvadim@users.noreply.github.com>
2023-04-18 13:20:44 +0300

    Add files via upload

---
+++ lab2/StudentDiscipline.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2
+{
+    //Клас для зв'язку між Студентами та Дисциплінами (багато-до-багатьох)
+    public class StudentDiscipline
+    {
+        public Students Student { get; set; }
+        public Discipline Discip { get; set; }
+
+        public StudentDiscipline() { }
+
+        public StudentDiscipline(Students stud, Discipline disc)
+        {
+            Student = stud;
+            Discip = disc;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"ID студента: {Student.StudentID}");
+            sb.AppendLine($"ID дисципліни: {Discip.DisciplineID}");
+            return sb.ToString();
+        }
+
+    }
+}
---
+++ lab2/Students.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization; 
+using System.Threading.Tasks;
+
+namespace lab2
+{
+    //Клас Студент 
+    public class Students
+    {
+        public int StudentID { get; set; }
+        public string FullName { get; set; }
+        public string Group { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public double AverageMark { get; set; }
+
+        //зв'язок з Класом HeadCurators один-до-багатьох(один Керівник допомагає готувати диплом багатьом студентам)
+        public HeadCurators Curator { get; set; }
+
+        public Students() { }
+        public Students(int studentId, string fullName, string group, DateTime dateOfBirth, double averageMark, HeadCurators curator)
+        {
+            StudentID = studentId;
+            FullName = fullName;
+            Group = group;
+            DateOfBirth = dateOfBirth;
+            AverageMark = averageMark;
+            Curator = curator;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"ID студента: {StudentID}");
+            sb.AppendLine($"ПІБ: {FullName}");
+            sb.AppendLine($"Група: {Group}");
+            sb.AppendLine($"Дата народження: {DateOfBirth.ToString("yyyy-MM-dd")}");
+            sb.AppendLine($"Середня оцінка: {AverageMark}");
+            sb.AppendLine($"Керівник: {Curator.FullName}");
+            return sb.ToString();
+        }
+    }
+}
