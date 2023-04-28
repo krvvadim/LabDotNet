cf30dc1e0c14c98bc138ca6cc3d6c31cebb988b8
Vadim Kruposiy <90095400+krvvadim@users.noreply.github.com>
2023-04-28 19:00:57 +0300

    Add files via upload

---
+++ lab3/InputOutput.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3
+{
+    public class InputOutput
+    {
+
+        public static Figure CreateSquare()
+        {
+            Console.Write("Enter side of the square: ");
+            int side = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            IFigureFactory squareFactory = new SquareFactory(side);
+            Figure square = squareFactory.CreateFigure();
+            return square;
+        }
+
+        public static Figure CreateCircle()
+        {
+            Console.Write("Enter radius for circle: ");
+            int radius = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            IFigureFactory circleFactory = new CircleFactory(radius);
+            Figure circle = circleFactory.CreateFigure();
+            return circle;
+        }
+
+        public static Figure CreateTriangle()
+        {
+            Console.Write("Enter side A of the triangle: ");
+            int side_A = int.Parse(Console.ReadLine());
+            Console.Write("Enter side B of the triangle: ");
+            int side_B = int.Parse(Console.ReadLine());
+            Console.Write("Enter side C of the triangle: ");
+            int side_C = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            IFigureFactory triangleFactory = new TriangleFactory(side_A, side_B, side_C);
+            Figure triangle = triangleFactory.CreateFigure();
+            return triangle;
+        }
+
+        public static Figure CreateParallelogram()
+        {
+            Console.Write("Enter side A of the parallelogram: ");
+            int side_A = int.Parse(Console.ReadLine());
+            Console.Write("Enter side B of the parallelogram: ");
+            int side_B = int.Parse(Console.ReadLine());
+            Console.Write("Enter height of the parallelogram: ");
+            int height = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            IFigureFactory parallelogramFactory = new ParalelogramFactory(side_A, side_B, height);
+            Figure parallelogram = parallelogramFactory.CreateFigure();
+            return parallelogram;
+        }
+
+        public static void OutputFigure(List<Figure> figures)
+        {
+            Console.WriteLine("\nІнформація про всі фігури:");
+            foreach (Figure figure in figures)
+            {
+                Console.WriteLine("Тип фігури: " + figure.GetType().Name);
+                Console.WriteLine("Площа: " + figure.GetArea());
+                Console.WriteLine("Периметр: " + figure.GetPerimeter() + "\n");
+            }
+        }
+
+        public static Figure CreateCompositeFigures()
+        {
+            List<Figure> selectedFigures = new List<Figure>();
+            int figureMenu;
+            do
+            {
+                Console.WriteLine("Choose a figure to add to the composite (or '5' to exit):");
+                Console.WriteLine("   1.square");
+                Console.WriteLine("   2.circle");
+                Console.WriteLine("   3.triangle");
+                Console.Write("   4.parallelogram: ");
+
+                figureMenu = int.Parse(Console.ReadLine());
+                switch (figureMenu)
+                {
+                    case 1:
+                        Figure square = InputOutput.CreateSquare();
+                        selectedFigures.Add(square);
+                        break;
+                    case 2:
+                        Figure circle = InputOutput.CreateCircle();
+                        selectedFigures.Add(circle);
+                        break;
+                    case 3:
+                        Figure triangle = InputOutput.CreateTriangle();
+                        selectedFigures.Add(triangle);
+                        break;
+                    case 4:
+                        Figure parallelogram = InputOutput.CreateParallelogram();
+                        selectedFigures.Add(parallelogram);
+                        break;
+                    case 5:
+                        break;
+                    default:
+                        Console.WriteLine("Invalid input. Please enter a valid figure type or 'e' to exit.");
+                        break;
+                }
+            } while (figureMenu != 5);
+            CompositeFigure compositeFigure = new CompositeFigure { Figures = selectedFigures };
+
+            Console.Write($"\nComposite figures: ");
+            foreach (Figure compFig in selectedFigures)
+            {
+                Console.Write(compFig.GetType().Name + " ");
+            }
+            Console.WriteLine("\nComposite figure area: " + compositeFigure.GetArea());
+            Console.WriteLine("Composite figure perimeter: " + compositeFigure.GetPerimeter());
+            return compositeFigure;
+        }
+    }
+}
---
+++ lab3/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab3
+{
+    /*Реалізувати задачу створення різних геометричних фігур. Повинно бути реалізовано створення декількох фігур з різними характеристиками та комбінацій фігур.*/
+    /*Будемо використовувати Factory method*/
+
+    // Використання фабричних методів для створення фігур
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+
+            int menu = 0;
+            List<Figure> figures = new List<Figure>();
+
+            do
+            {
+                Console.WriteLine("Menu: ");
+                Console.WriteLine("   1. Створити квадрат");
+                Console.WriteLine("   2. Створити коло");
+                Console.WriteLine("   3. Створити трикутник");
+                Console.WriteLine("   4. Створити паралелограм");
+                Console.WriteLine("   5. Показати інформацію про всі фігури");
+                Console.WriteLine("   6. Створити складну фігуру з вказаних");
+                Console.Write("   7. Вийти: ");
+
+                try
+                {
+                    menu = int.Parse(Console.ReadLine());
+
+                    switch (menu)
+                    {
+                        case 1:
+                            Figure square = InputOutput.CreateSquare();
+                            figures.Add(square);
+                            break;
+                        case 2:
+                            Figure circle = InputOutput.CreateCircle();
+                            figures.Add(circle);
+                            break;
+                        case 3:
+                            Figure triangle = InputOutput.CreateTriangle();
+                            figures.Add(triangle);
+                            break;
+                        case 4:
+                            Figure parallelogram = InputOutput.CreateParallelogram();
+                            figures.Add(parallelogram);
+                            break;
+                        case 5:
+                            InputOutput.OutputFigure(figures);
+                            break;
+                        case 6:
+                            Figure compositeFigures = InputOutput.CreateCompositeFigures();
+                            figures.Add(compositeFigures);
+                            break;
+                        case 7:
+                            Console.WriteLine("До побачення!");
+                            break;
+                        default:
+                            Console.WriteLine("Невірне значення. Введіть число від 1 до 7.");
+                            break;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+            } while (menu != 7);
+        }
+            
+    }
+
+}
